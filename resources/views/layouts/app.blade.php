fb9f381b2e7ebef3d7ad0981d6019c0d15aa6b8c
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-26 23:22:59 +0300

    main register form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <ul class="flex items-center">
         <li><a href="" class="p-3">Morshinin</a></li>
         <li><a href="" class="p-3">Login</a></li>
-        <li><a href="" class="p-3">Register</a></li>
+        <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
         <li><a href="" class="p-3">Logout</a></li>
     </ul>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/register', [RegisterController::class,
+    'index'])->name('register');
 
 Route::get('/posts', function () {
     return view('posts.index');
