1628d37f1c4cd1ab44da053b266ded6445f2e37e
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-27 02:53:56 +0300

    main create dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/dashboard', [DashboardController::class,
+    'index'])->name('dashboard');
 
 Route::get('/register', [RegisterController::class,
     'index'])->name('register');
