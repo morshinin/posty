f072747232c0d664ee377d28114e5fc701040de3
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-28 01:35:31 +0300

    main posting functionality

--- database/migrations/2020_12_27_185213_create_posts_table.php
+++ database/migrations/2020_12_27_185213_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('body');
             $table->timestamps();
         });
