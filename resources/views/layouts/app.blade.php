f9a1411dafcb3046c1b126fb6a8111ea01cc3337
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-27 18:05:44 +0300

    main login functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,15 @@
         <li><a href="" class="p-3">Post</a></li>
     </ul>
     <ul class="flex items-center">
-        <li><a href="" class="p-3">Morshinin</a></li>
-        <li><a href="" class="p-3">Login</a></li>
-        <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
-        <li><a href="" class="p-3">Logout</a></li>
+        @auth
+            <li><a href="" class="p-3">Morshinin</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        @endauth
+
+        @guest
+            <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
+            <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+        @endguest
     </ul>
 </nav>
     @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,19 @@
 
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class,
     'index'])->name('dashboard');
 
+Route::get('/login', [LoginController::class,
+    'index'])->name('login');
+Route::post('/login', [LoginController::class,
+    'store']);
+
 Route::get('/register', [RegisterController::class,
     'index'])->name('register');
-
 Route::post('/register', [RegisterController::class,
     'store']);
 
