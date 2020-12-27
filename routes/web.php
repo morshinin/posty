633a06102f293bbbe9f27804efd8c5c5fa054036
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-27 21:49:56 +0300

    main posts controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,6 +27,7 @@ Route::get('/register', [RegisterController::class,
 Route::post('/register', [RegisterController::class,
     'store']);
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class,
+    'index'])->name('posts');
+Route::post('/posts', [PostController::class,
+    'store']);
