aa30e8fcef19ba1de36674bcc4be6d6355a61505
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-29 23:13:05 +0300

    main received likes

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,20 +3,33 @@
 @section('content')
 
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-
-            @if ($posts->count())
-                @foreach($posts as $post)
-                    <x-post :post="$post" />
-                @endforeach
-
-                {{ $posts->links() }}
-
-            @else
+        <div class="w-8/12">
+            <div class="p-6">
+                <h1 class="text-2xl font-medium mb-1">
+                    {{ $user->username }}
+                </h1>
                 <p>
-                    {{ $user->username }} does not have any posts
+                    Posted {{ $posts->count() }}
+                    {{ Str::plural('post', $posts->count()) }}
+                    and received
+                    {{ $user->receivedLikes->count() }} likes
                 </p>
-            @endif
+            </div>
+
+            <div class="bg-white p-6 rounded-lg">
+                @if ($posts->count())
+                    @foreach($posts as $post)
+                        <x-post :post="$post"/>
+                    @endforeach
+
+                    {{ $posts->links() }}
+
+                @else
+                    <p>
+                        {{ $user->username }} does not have any posts
+                    </p>
+                @endif
+            </div>
         </div>
     </div>
 
