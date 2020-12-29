3493dd44a85f07734d20f7ad01592c902d26e73c
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-29 19:16:43 +0300

    main unliking functionality

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class PostLikeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function store(Post $post, Request $request)
     {
         if ($post->likedBy(($request->user()))) {
@@ -16,6 +21,13 @@ class PostLikeController extends Controller
         $post->likes()->create([
             'user_id'   => $request->user()->id,
         ]);
+
+        return back();
+    }
+
+    public function destroy(Post $post, Request $request)
+    {
+        $request->user()->likes()->where('post_id', $post->id)->delete();
 
         return back();
     }
