f26709e886b1678d56954137f074aca4fa06f1f6
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-29 17:25:45 +0300

    main like functionality

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -31,3 +32,6 @@ Route::get('/posts', [PostController::class,
     'index'])->name('posts');
 Route::post('/posts', [PostController::class,
     'store']);
+
+Route::post('/posts/{post}/likes', [PostLikeController::class,
+    'store'])->name('posts.likes');
