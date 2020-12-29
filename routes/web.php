18262bb257aeeea752964bc91cee62d829b307d2
m.morshinin <m.morshinin@b2b-center.ru>
2020-12-29 22:40:47 +0300

    main users posts page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostLikeController;
+use App\Http\Controllers\UserPostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,9 @@ Route::get('/dashboard', [DashboardController::class,
 
 Route::post('/logout', [LogoutController::class,
     'store'])->name('logout');
+
+Route::get('/users/{user:username}/posts', [UserPostController::class,
+    'index'])->name('users.posts');
 
 Route::get('/login', [LoginController::class,
     'index'])->name('login');
